68986cd82389fc33b885d0c73f664f345ae131fe
Christian Alamo <christian.alamo@powen.es>
2023-03-16 23:35:53 +0100

    chore: added to npm

--- test.actions.ts
+++
@@ -1,9 +0,0 @@
-console.log('Hi test Frontend Rules');
-import { createAction } from '@ngrx/store';
-
-export const gettest = createAction('[test] Get test');
-
-export const gettestSuccessfully = createAction(
-    '[test] Get test successfully',
-    props<{ test: Test[] }>()
-);
